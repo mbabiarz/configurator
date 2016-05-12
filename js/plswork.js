45218db916e95e5a9fdce6630a558aa39e478ded
Margaret Babiarz <sspole@gmail.com>
2016-05-12 10:17:33 -0600

    react refactor working!

--- js/plswork.js
+++ js/plswork.js
@@ -4,18 +4,21 @@ var App = React.createClass({
       liked: false,
       pipeSize: '2 in.',
       hoseSize: '4/4',
-      flangeYes: true,
-      flangeNo: false,
-      splashPlate: true
+      flange: true,
+      plate: true
     };
-  },
-  
-  handleClick: function(event) {
-    this.setState({liked: !this.state.liked});
   },
   
   handleCheck: function(e) {
     this.setState({isChecked: !this.state.isChecked});
+  },
+  
+  updateFlange: function (nextState) {
+    this.setState(nextState);
+  },
+  
+  updatePlate: function (nextState) {
+    this.setState(nextState);
   },
   
   setPipeSize: function(e) {
@@ -27,75 +30,83 @@ var App = React.createClass({
     this.setState({ hoseSize: e.target.value });
   },
   
-  setRadio: function(e) {
-    var val = e.currentTarget;
-    var newVal = {};
-    newVal[val.id] = val.checked;
-    this.setState(newVal);
-    console.log(flangeYes.value);
-    console.log(flangeNo.value);
-  },
+//  Matt's checkbox example
+//  toggleValue: function (event) {
+//    var val = event.currentTarget;
+//    var newVal = {};
+//    newVal[val.id] = val.checked;
+//    this.setState(newVal);
+//  },
   
-  toggleValue: function (event) {
-    var val = event.currentTarget;
-    var newVal = {};
-    newVal[val.id] = val.checked;
-    this.setState(newVal);
-  },
+  
+  //*****  RENDERING
   
   render: function() {
     var text = this.state.liked ? 'like' : 'haven\'t liked';
-    var inlineStyle = { display:'inline' };
+    var styleInline = { display:'inline' };
     
     return (      
       <div>
         <h2>Configure your tool</h2>
         
-         <div>
+        {/* PIPE SIZE */}
+        <div>
           <label htmlFor="pipeSize">Pipe Size</label>
-         <select id="pipeSize" defaultValue={this.state.pipeSize} onChange={this.setPipeSize}>
+          <select id="pipeSize" defaultValue={this.state.pipeSize} onChange={this.setPipeSize}>
           <option value='2 in.'>2 in.</option>
           <option value='2.5 in.'>2.5 in.</option>
           <option value='3 in.'>3 in.</option>
           <option value='4 in.'>4 in.</option>
-         </select>
-         </div>
+          </select>
+        </div>
       
-         <div>
-         <label htmlFor="hoseSize"> Hose Size</label>
-         <select id="hoseSize" defaultValue={this.state.hoseSize} onChange={this.setHoseSize}>
+        {/* HOSE SIZE */}
+        <div>
+          <label htmlFor="hoseSize"> Hose Size</label>
+          <select id="hoseSize" defaultValue={this.state.hoseSize} onChange={this.setHoseSize}>
           <option value="4/4">4/4</option>
           <option value="5/4">5/4</option>
           <option value="6/4">6/4</option>
-         </select>
+          </select>
          </div>
       
-         <div>
-          <p style={inlineStyle}><strong>Does the pipe have a flange?</strong></p>
-          <input name="flange" id="flangeYes" defaultChecked type="radio" onChange={this.setRadio} />
-          <label htmlFor="flangeYes"> Yes</label>
-          <input name="flange" id="flangeNo" type="radio" onChange={this.setRadio} />
-          <label htmlFor="flangeNo"> No</label>
+        {/* FLANGE */}
+        <div>
+          <p style={styleInline}><strong>Does the pipe have a flange?</strong></p>
+          <input type="radio" name="flange" onClick={this.updateFlange.bind(null, {flange: true})} value="true" checked={this.state.flange} />
+          <label> Yes</label>
+          <input type="radio" name="flange" onClick={this.updateFlange.bind(null, {flange: false})} value="false" checked={!this.state.flange} />
+          <label> No</label>
         </div>
         
-        {this.state.flangeNo && 
+        {/* PLATE */}                    
+        {!this.state.flange && (
+          <div>
+            <p style={styleInline}><strong>Will you reuquire a splash plate?</strong></p>
+            <input type="radio" name="plate" onClick={this.updatePlate.bind(null, {plate: true})} value="true" checked={this.state.plate} />
+            <label> Yes</label>
+            <input type="radio" name="plate" onClick={this.updatePlate.bind(null, {plate: false})} value="false" checked={!this.state.plate} />
+            <label> No</label>
+          </div>
+        )}
+
+        
+        {/* Matt's checkbox                     
+        {!this.state.flange && (
           <div>
             <input id="splashPlate" defaultChecked={this.state.splashPlate} type="checkbox" onChange={this.toggleValue}/>
             <label htmlFor="splashPlate">Splash Plate</label>
           </div>
-        }
-        
+        )} */}
 
+
+        
+        {/* RESULTS */}
         <h2>Results</h2>
         <p>Pipe Size: {this.state.pipeSize}</p>
         <p>Hose Size: {this.state.hoseSize}</p>
-        {this.state.flangeYes && <p>Flange: yes</p>}
-        {this.state.flangeNo && this.state.splashPlate && <p>Splash plate: yes</p>}
-      
-        <h2>Toggle Experiments</h2>
-        <p onClick={this.handleClick}>
-          You {text} this. Click to toggle.
-        </p>
+        <p>{this.state.flange ? 'Flange: yes' : 'Flange: no'}</p>
+        {!this.state.flange && this.state.plate ? 'Plate: yes' : 'Plate: no'}
       </div>
     );
   }
